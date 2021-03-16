9a6e1599064b0938c468b74b71deb5d63e7826e1
flintco <connor.flint16@gmail.com>
2021-03-16 16:44:04 -0500

    Gets current temperature in Madison from API

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -10,11 +10,11 @@ class DailyForecast extends Component {
     }
 
     componentDidMount(){
-      fetch('https://randomuser.me/api')
+      fetch('https://api.openweathermap.org/data/2.5/weather?lat=43.07&lon=-89.38&appid=48d41b2d7115ee18536bc93db9915079')
         .then(res=>res.json())
         .then(data=>{
           this.setState({
-            temperature: data.results[0].gender
+            temperature: ((9 * (data.main.temp - 273) / 5) + 32).toFixed(0)
           });
         });
     }
