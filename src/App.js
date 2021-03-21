ff6880f81ff1a8277e4158812ee30048230afd4b
flintco <connor.flint16@gmail.com>
2021-03-21 13:47:35 -0500

    First CSS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { buildQueries } from '@testing-library/dom';
 import React, { Component } from 'react';
 import './App.css';
 import DailyForecast from './DailyForecast';
 
 class App extends Component {
   render() {
+    const ForecastStyles = {
+      backgroundColor: "blue",
+      width: "100px",
+      height: "100px",
+      float: "left"
+    };
     return (
       <div className="App">
-        <DailyForecast location="Madison, WI" lat="43.07" long="-89.38"></DailyForecast>
-        <DailyForecast location="Chicago, IL" lat="41.88" long="-87.62"></DailyForecast>
-        <DailyForecast location="Minneapolis, MN" lat="44.98" long="-93.27"></DailyForecast>
-        <DailyForecast location="Milwaukee, WI" lat="43.03" long="-87.92"></DailyForecast>
-        <DailyForecast location="Detroit, MI" lat="42.33" long="-83.05"></DailyForecast>
+        <h1 style={{color: "red"}}>Hello</h1>
+        <DailyForecast location="Madison, WI" lat="43.07" long="-89.38" style={{color: "red"}}></DailyForecast>
+        <DailyForecast style={ForecastStyles} location="Chicago, IL" lat="41.88" long="-87.62"></DailyForecast>
+        <DailyForecast style={ForecastStyles} location="Minneapolis, MN" lat="44.98" long="-93.27"></DailyForecast>
+        <DailyForecast style={ForecastStyles} location="Milwaukee, WI" lat="43.03" long="-87.92"></DailyForecast>
+        <DailyForecast style={ForecastStyles} location="Detroit, MI" lat="42.33" long="-83.05"></DailyForecast>
       </div>
     );
 }
