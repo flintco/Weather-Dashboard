98bac5d62f7e3619aa7d9e80544fb7aff713bbe7
flintco <connor.flint16@gmail.com>
2021-03-30 15:15:35 -0500

    Conditional Image produces image based on props

--- src/ConditionalImage.js
+++ src/ConditionalImage.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import snow from './Images/snow.png'
+import cloudy from './Images/cloudy.png';
+import lightning from './Images/lightning.png';
+import partlycloudy from './Images/partlycloudy.png';
+import rainy from './Images/rainy.png';
+import snow from './Images/snow.png';
+import sun from './Images/sun.png';
 
 class ConditionalImage extends Component {
     constructor (props){
       super(props);
-      this.state = {
-        imageAddress: ""
-      };
     }
 
     render(){
-       /* if (this.props.weather == "snow"){
-            this.setState({
-                imageAddress: "./Images/snow.png"
-            });
-        } 
-        {this.state.imageAddress}
-        */
-       var imageName = require('./Images/snow.png')
+        var source;
+        if (this.props.weather == "cloudy"){
+            source = cloudy
+        } else if (this.props.weather == "lightning"){
+            source = lightning
+        } else if (this.props.weather == "partlycloudy"){
+            source = partlycloudy
+        } else if (this.props.weather == "rainy"){
+            source = rainy
+        } else if (this.props.weather == "snow"){
+            source = snow
+        } else {
+            source = sun
+        }
         return(
             <div>
-                <img src={snow} style={{width: "100px"}}/>
+                <img src={source} style={{width: "100px", height:"100px"}}/>
             </div>
         )
     }
